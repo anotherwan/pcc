7f97d2e4aeb6fc232222f919fc1457c5939d115b
swan <itsanotherwan@gmail.com>
2021-06-21 13:58:47 -0400

    Logic in place
    Added JS logic to display correct questions

--- components/questions.php
+++ components/questions.php
@@ -1,13 +1,25 @@
 <?php
 if (!!$c['question_slides']) :
     $qSlide = $c['question_slides'];
-    foreach ($qSlide as $index => $q) :       
+    foreach ($qSlide as $index => $q) :   
 ?>
-    <div class="question" id="question-<?php echo $index ?>">
-        <?php
-            echo $q['questions'];
-        ?>
+<li class="slide">
+    <!-- <div id="q-anim"> -->
+    <div id="q-anim" class="bodymovin" data-bm-path="<?php echo get_template_directory_uri(); ?>/assets/animations/question.json" 
+    data-bm-renderer="svg" data-name="qanim" data-bm-loop="false">
     </div>
+        <ul class="questions">
+            <li class="question" id="question-<?php echo ($index+1) ?>">
+                <?php
+                    echo $q['questions'];
+                ?>
+            </li>
+            <div class="q-number">
+                <?php echo $index+1; ?>
+            </div>
+        </ul>
+    
+</li>
 <?php
     endforeach;
 endif;
--- templates/template-home.php
+++ templates/template-home.php
@@ -4,33 +4,35 @@
     **/
     get_header();
 ?>
-<div class="wrapper" id="intro-wrapper">
-    <div id="intro-anim" class="bodymovin" data-bm-path="<?php echo get_template_directory_uri(); ?>/assets/animations/question_intro.json" data-bm-renderer="svg" data-bm-loop="false">
-        <p class="names">
-        <?php 
-            $args_names = [
-                'intro_slide' => get_field('intro_slide'),
-                'names' => get_field('names'),
-                'name' => get_field('name'),
-            ];
-            echo get_component('components/names', $args_names); 
-        ?>
-        </p>
-    </div>
-</div>
-
-<div class="wrapper" id="questions-wrapper">
-    <div id="q-anim" class="bodymovin" data-bm-path="<?php echo get_template_directory_uri(); ?>/assets/animations/question.json" data-bm-renderer="svg" data-bm-loop="false">
-        <div class="questions">
+<ul class="slides">
+    <li class="slide current">
+        <div id="intro-anim" class="bodymovin" data-bm-path="<?php echo get_template_directory_uri(); ?>/assets/animations/question_intro.json" data-bm-renderer="svg" data-bm-loop="false">
+            <p class="names">
             <?php 
-                $args_questions = [
-                    'question_slides' => get_field('question_slides'),
-                    'questions' => get_field('questions')
+                $args_names = [
+                    'intro_slide' => get_field('intro_slide'),
+                    'names' => get_field('names'),
+                    'name' => get_field('name'),
                 ];
-                echo get_component('components/questions', $args_questions);
+                echo get_component('components/names', $args_names); 
             ?>
+            </p>
         </div>
-    </div>
-</div>
+    </li>
+    <!-- <li class="slide"> -->
+        <!-- <div id="q-anim" class="bodymovin" data-bm-path="<?php //echo get_template_directory_uri(); ?>/assets/animations/question.json" data-bm-renderer="svg" data-bm-loop="false"></div> -->
+            <!-- <ul class="questions"> -->
+                <?php 
+                    $args_questions = [
+                        'question_slides' => get_field('question_slides'),
+                        'questions' => get_field('questions')
+                    ];
+                    echo get_component('components/questions', $args_questions);
+                ?>
+            <!-- </ul>
+        </div> -->
+    <!-- </li> -->
+
+</ul>
 
 <?php get_footer(); ?>
